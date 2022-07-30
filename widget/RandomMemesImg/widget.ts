0aee40d2bd0ccc155f97438ad1f255f4e5b9b32c
litwak913 <litwak913@qq.com>
2022-07-30 17:48:33 +0800

    feat:随机梗图微件

---
+++ widget/RandomMemesImg/widget.ts
@@ -0,0 +1,27 @@
+/*
+ * @Author: litwak913
+ * @Date: 2022-07-30 16:41:58
+ * @LastEditors: litwak913 litwak913@qq.com
+ * @LastEditTime: 2022-07-30 17:47:24
+ * @FilePath: /mcbbs-wiki-widget-repo/widget/RandomMemesImg/widget.ts
+ * @Description: RandomMemesImg 显示梗图
+ */
+import { docReady } from 'Utils/utils'
+import { MemeImg } from './memeImg'
+// 你可能需要一些工具函数，比如这个DOM加载完毕后执行代码的方法
+docReady(async () => {
+  const memeRes = await fetch('https://mcbbs.wiki/913-api/imgs?type=json')
+  const meme: MemeImg = await memeRes.json()
+  console.log(meme)
+  const message = document.getElementById('memesimg')
+  const loading = document.getElementById('memes-loading')
+  if (message) {
+    const link = document.createElement('a')
+    const image = document.createElement('img')
+    image.src = `https://mcbbs.wiki/${meme.path}`
+    link.href = `https://mcbbs.wiki/wiki/${meme.page}`
+    loading?.remove()
+    link.appendChild(image)
+    message.appendChild(link)
+  }
+})
--- widget/index.tsx
+++ widget/index.tsx
@@ -1,17 +1,19 @@
 /*
  * @Author: Salt
  * @Date: 2022-07-23 15:48:30
- * @LastEditors: Salt
- * @LastEditTime: 2022-07-24 17:51:38
+ * @LastEditors: litwak913 litwak913@qq.com
+ * @LastEditTime: 2022-07-30 17:47:56
  * @Description: 这个文件的功能
  * @FilePath: \mcbbs-wiki-widget-repo\widget\index.tsx
  */
 import SaltOutsideMusicLoader from './SaltOutsideMusicLoader'
 import SaltFirework from './SaltFirework'
 import getMCBBSCredit from './getMCBBSCredit'
+import RandomMemesImg from './RandomMemesImg'
 
 export default [
   { title: 'SaltOutsideMusicLoader', Component: SaltOutsideMusicLoader },
   { title: 'SaltFirework', Component: SaltFirework },
   { title: 'getMCBBSCredit', Component: getMCBBSCredit },
+  { title: 'RandomMemesImg', Component: RandomMemesImg },
 ] as { title: string; Component: () => JSX.Element }[]
