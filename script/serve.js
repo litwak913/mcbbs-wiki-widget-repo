fd9fdb333577e1a61029e964267793ceee9dd2b4
Salt <732237136@qq.com>
2022-07-30 21:24:54 +0800

    fix: 修复本地开发服务器，区分linux和win、win11下能自动打开网页

--- script/serve.js
+++ script/serve.js
@@ -1,5 +1,5 @@
 /*
- * @LastEditTime: 2022-07-23 16:48:11
+ * @LastEditTime: 2022-07-30 21:14:20
  * @Description: 本地热更新服务
  */
 const port = 5000
@@ -9,12 +9,11 @@ const core = require('./tools/core')
 const $P = require('./tools/format-print')
 const $T = require('./tools/format-time')
 const { findPort } = require('./tools/port')
-const { createServer } = require('http')
-const sirv = require('sirv')
-const open = require('open')
+const openUrl = require('./tools/openUrl')
 
 console.log($P('MCBSSWiki widget - serve ' + $T(), 'grey'))
 ;(async () => {
+  const www = path.resolve(__dirname, '../www')
   const _port = await findPort(port)
   const url = `http://localhost:${_port}/`
   const onRebuild = (error) => {
@@ -43,9 +42,17 @@ console.log($P('MCBSSWiki widget - serve ' + $T(), 'grey'))
     },
   })
   console.log($P(' SERVE ', 'b', 'white', 'cyanbg'), '编译完成, 开启服务中')
-  const server = sirv(path.resolve(__dirname, '../www'), { dev: true })
-  createServer((req, res) => server(req, res)).listen(_port)
-  open(url)
+  if (process.platform === 'linux') {
+    // https://github.com/rsms/serve-http/issues/6
+    const { createServer } = require('http')
+    const sirv = require('sirv')
+    const server = sirv(www, { dev: true })
+    createServer((req, res) => server(req, res)).listen(_port)
+  } else {
+    const { createServer } = require('serve-http')
+    createServer({ port: _port, pubdir: www, quiet: true })
+  }
+  openUrl(url)
   console.log(
     $P(' SERVE ', 'b', 'white', 'greenbg'),
     '服务开启, 见:',
