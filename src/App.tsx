9a9a1a3eabf1566782c91fee08c2122d52279a68
litwak913 <litwak913@qq.com>
2022-08-05 17:05:44 +0800

    feat:切换

--- src/App.tsx
+++ src/App.tsx
@@ -6,37 +6,45 @@
  * @Description: 这个文件的功能
  * @FilePath: \mcbbs-wiki-widget-repo\src\App.tsx
  */
-import React from 'react'
+import React, { useState } from 'react'
 import widget from 'Widget/index'
 import { addScript } from 'Utils/resource'
 import { TextIcon } from './components/TextIcon'
 
 addScript('https://cdn.bootcss.com/jquery/2.2.2/jquery.js', false, 'jquery')
-function change(event:any) {console.log(event.target.value)}
-export default () => (
+export default () => { 
+  const [current,setCurrent]=useState(0)
+  function change(event: any) {
+    console.log(event.target.value)
+    setCurrent(event.target.value)
+  }
+  return(
   <div>
-    <select name="widget" onChange={change}>
-      {widget.map(({ title }) => (
-        <option value={title}>{title}</option>
+    <select name="widget" onChange={change} defaultValue={0}>
+      {widget.map(({ title }, index) => (
+        <option value={index} key={index}>
+          {title}
+        </option>
       ))}
     </select>
     <div className="mcbbs-wiki-widget-list">
-      {widget.map(({ title, Component }) => (
+    {widget.map(({ title, Component },index)=>{
+    return(
         <div key={title} className="mcbbs-wiki-widget-item">
-          <h2>
-            <a
-              href={`//mcbbs.wiki/wiki/Widget:${title}`}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              
-              <TextIcon txt="微件" />
-              {title}
-            </a>
-          </h2>
-          <Component />
-        </div>
-      ))}
+        <h2>
+          <a
+            href={`//mcbbs.wiki/wiki/Widget:${title}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <TextIcon txt="微件" />
+            {title}
+          </a>
+        </h2>
+        <Component />
+      </div>
+    )
+  })}
     </div>
   </div>
-)
+)}
