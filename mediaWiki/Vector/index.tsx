f4edf7c3174f9d26eeb4c5a203e2967e546f0f47
Salt <732237136@qq.com>
2022-08-13 00:21:19 +0800

    feat: 旧版代码拆了出来

--- mediaWiki/Vector/index.tsx
+++ mediaWiki/Vector/index.tsx
@@ -2,7 +2,7 @@
  * @Author: Salt
  * @Date: 2022-08-11 21:45:33
  * @LastEditors: Salt
- * @LastEditTime: 2022-08-12 23:41:30
+ * @LastEditTime: 2022-08-13 00:05:49
  * @Description: 这个文件的功能
  * @FilePath: \mcbbs-wiki-widget-repo\mediaWiki\Vector\index.tsx
  */
@@ -12,6 +12,15 @@ import { copy } from 'Utils/utils'
 import commonStyle from './common.t.scss'
 import newStyle from './new.t.scss'
 import oldStyle from './old.t.scss'
+
+const ClickCopy = (props: { txt: string }) => (
+  <small
+    style={{ cursor: 'pointer', padding: '1px 2px', border: '1px dashed #eee' }}
+    onClick={() => copy(props.txt)}
+  >
+    复制
+  </small>
+)
 
 export default function Vector() {
   const throwSourceMap = (str: string) =>
@@ -25,23 +34,17 @@ export default function Vector() {
     <>
       <h3>
         通用部分
-        <small style={{ cursor: 'pointer' }} onClick={() => copy(commonS)}>
-          复制
-        </small>
+        <ClickCopy txt={commonS} />
       </h3>
       <pre style={{ columnCount: 3 }}>{commonS}</pre>
       <h3>
         旧版
-        <small style={{ cursor: 'pointer' }} onClick={() => copy(oldS)}>
-          复制
-        </small>
+        <ClickCopy txt={oldS} />
       </h3>
       <pre style={{ columnCount: 3 }}>{oldS}</pre>
       <h3>
         新版
-        <small style={{ cursor: 'pointer' }} onClick={() => copy(newS)}>
-          复制
-        </small>
+        <ClickCopy txt={newS} />
       </h3>
       <pre style={{ columnCount: 3 }}>{newS}</pre>
     </>
