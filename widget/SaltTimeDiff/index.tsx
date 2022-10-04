4acbb7d767cc0ef9fe004ce072982ebf3009855e
Salt <732237136@qq.com>
2022-10-04 18:53:57 +0800

    feat: 添加了年份、月份计算，支持 UTC 时间戳

--- widget/SaltTimeDiff/index.tsx
+++ widget/SaltTimeDiff/index.tsx
@@ -2,7 +2,7 @@
  * @Author: Salt
  * @Date: 2022-10-04 14:17:22
  * @LastEditors: Salt
- * @LastEditTime: 2022-10-04 16:16:40
+ * @LastEditTime: 2022-10-04 18:22:39
  * @Description: 这个文件的功能
  * @FilePath: \mcbbs-wiki-widget-repo\widget\SaltTimeDiff\index.tsx
  */
@@ -10,6 +10,23 @@
 import React from 'react'
 import './widget'
 import './index.scss'
+
+const ToNow = (props: { time: string; time2?: string; cmd?: string }) => (
+  <>
+    <hr />
+    {props.time} 距 {props.time2 || '今天'}：
+    <br />
+    <span className={`salt-time-diff ${props.time2 ? '' : 'real-time'}`}>
+      {props.time}SPLIT{props.time2 || ''}SPLIT{props.cmd || '年月d小时分钟秒M'}
+    </span>
+    {' | '}
+    <span
+      className={`salt-time-diff ${props.time2 ? '' : 'real-time'} complex`}
+    >
+      {props.time}SPLIT{props.time2 || ''}SPLIT{props.cmd || '年月d小时分钟秒M'}
+    </span>
+  </>
+)
 
 export default () => {
   // 可能需要的一些逻辑
@@ -40,27 +57,21 @@ export default () => {
       <span className="salt-time-diff real-time complex">
         1949/10/1 15:00 SPLIT SPLIT d小时分钟秒M
       </span>
-      <br />
-      <span className="salt-time-diff real-time complex">
-        1949/10/1 15:00 SPLIT SPLIT d小时分钟秒
-      </span>
+      <ToNow time="2022年9月1日" />
+      <ToNow time="2022年9月30日" />
+      <ToNow time="2021年10月30日 14:15:16" />
+      <ToNow time="2050年10月1日" />
+      <ToNow time="2050年10月1日" time2="2022年10月2日 14:15:16" />
+      <ToNow
+        time="2050年10月1日"
+        time2="2022年10月2日 14:15:16"
+        cmd="天时分秒"
+      />
       <hr />
-      2050年10月1日 距 今天：
+      使用 UTC 时间戳 1589552138000：
       <br />
-      <span className="salt-time-diff real-time complex">
-        2050年10月1日 SPLIT SPLIT d小时分钟秒M
-      </span>
-      <hr />
-      2050年10月1日 距 2022年10月1日：
-      <br />
-      <span className="salt-time-diff complex">
-        2050年10月1日 SPLIT 2022年10月1日 SPLIT d小时分钟秒M
-      </span>
-      <br />
-      2022年10月1日 距 2050年10月1日：
-      <br />
-      <span className="salt-time-diff complex">
-        2022年10月1日 SPLIT 2050年10月1日 SPLIT d小时分钟秒M
+      <span className="salt-time-diff real-time utc complex">
+        1589552138000 SPLIT SPLIT d小时分钟秒M
       </span>
     </>
   )
--- widget/SaltTimeDiff/widget.ts
+++ widget/SaltTimeDiff/widget.ts
@@ -2,7 +2,7 @@
  * @Author: Salt
  * @Date: 2022-10-04 14:17:22
  * @LastEditors: Salt
- * @LastEditTime: 2022-10-04 16:10:38
+ * @LastEditTime: 2022-10-04 18:15:53
  * @Description: 这个文件的功能
  * @FilePath: \mcbbs-wiki-widget-repo\widget\SaltTimeDiff\widget.ts
  */
@@ -11,68 +11,144 @@ import { docReady } from 'Utils/utils'
 //! <span class="salt-time-diff
 //! 实时更新则额外加上 real-time ——结束时间会强制改为当前时间
 //! 更复杂的显示请额外加上 complex ——可以用CSS修改渲染样式
+//! 如果时间是UTC请额外加上 utc
 //! ">{起始时间}SPLIT{结束时间}SPLIT{指令（d-天，h-小时，m-分钟，s-秒，M-毫秒）}</span>
 
+const UTCOffset = new Date().getTimezoneOffset() * 60 * 1000
 const cmdAttr = 'data-salt-time-diff-command'
 const startAttr = 'data-salt-time-diff-start'
 const endAttr = 'data-salt-time-diff-end'
-
-function getDate(time?: string | null) {
+const i18n = {
+  year: '年',
+  month: '个月',
+  day: '天',
+  hour: '小时',
+  minute: '分钟',
+  second: '秒',
+  ms: '毫秒',
+}
+/** 获取Date对象，如果输入非法则返回当前时间，如果输入为纯数字且为UTC时间则会自动处理时区 */
+function getDate(time?: string | null, utc = false) {
   if (!time) return new Date()
   // 毫秒数
   if (/^\d+$/.test(time.trim())) {
     const d = new Date(+time)
-    if (!isNaN(d.valueOf())) return d
-    else return new Date()
+    if (!isNaN(d.valueOf())) {
+      if (utc) d.setTime(d.getTime() + UTCOffset)
+      return d
+    } else return new Date()
   }
   // 其他情况
   const d = new Date(time)
   if (!isNaN(d.valueOf())) return d
   else return new Date()
 }
+interface ReadableTimeObj {
+  isBefore: boolean
+  year?: number
+  month?: number
+  day?: number
+  hour?: number
+  minute?: number
+  second?: number
+  ms?: number
+}
 /** 生成输出内容 */
-function timeDiff(_ms = 0, cmd = 'd', cpx = false) {
+function timeDiff({
+  t1,
+  t2,
+  cmd = 'd',
+  cpx = false,
+}: {
+  t1: Date
+  t2: Date
+  cmd?: string
+  cpx?: boolean
+}) {
+  const _ms = t1.valueOf() - t2.valueOf()
+  const isBefore = t1.valueOf() - t2.valueOf() > 0
+  const d1 = new Date(isBefore ? t2 : t1) // 小
+  const d2 = new Date(isBefore ? t1 : t2) // 大
+  // console.log(d1, d2);
   let ms = Math.abs(_ms)
-  const isBefore = _ms > 0
-  let res = cpx
-    ? isBefore
-      ? '<span class="salt-time-diff-txt salt-time-diff-before">还有</span>'
-      : '<span class="salt-time-diff-txt salt-time-diff-after">已过去</span>'
-    : ''
+  const diff = { isBefore } as ReadableTimeObj
+  if (cmd.indexOf('y') != -1) {
+    let years = d2.getFullYear() - d1.getFullYear()
+    d1.setFullYear(d2.getFullYear())
+    if (d1.getTime() > d2.getTime()) {
+      years -= 1
+      d1.setFullYear(d1.getFullYear() - 1)
+    }
+    ms = d2.getTime() - d1.getTime() // 抹平年份差异
+    diff.year = years
+  }
+  if (cmd.indexOf('o') != -1) {
+    let years = d2.getFullYear() - d1.getFullYear() // 抹平年份差异
+    d1.setFullYear(d2.getFullYear())
+    if (d1.getTime() > d2.getTime()) {
+      years -= 1
+      d1.setFullYear(d1.getFullYear() - 1)
+    }
+    // 计算月份
+    let months = d2.getMonth() - d1.getMonth()
+    if (d2.getFullYear() - d1.getFullYear()) months += 12
+    d1.setFullYear(d2.getFullYear())
+    d1.setMonth(d2.getMonth())
+    if (d1.getTime() > d2.getTime()) {
+      months -= 1
+      d1.setMonth(d1.getMonth() - 1)
+    }
+    ms = d2.getTime() - d1.getTime() // 抹平月份差异
+    diff.month = years * 12 + months
+  }
   if (cmd.indexOf('d') != -1) {
     let days = Math.floor(ms / (24 * 3600 * 1000))
     ms = ms % (24 * 3600 * 1000) // 除去天数后剩余的毫秒数
-    res += cpx
-      ? `<span class="salt-time-diff-res salt-time-diff-res-day">${days}</span>天`
-      : `${days}天`
+    diff.day = days
   }
   if (cmd.indexOf('h') != -1) {
     let hours = Math.floor(ms / (3600 * 1000))
     ms = ms % (3600 * 1000) // 除去小时后剩余的毫秒数
-    res += cpx
-      ? `<span class="salt-time-diff-res salt-time-diff-res-hour">${hours}</span>小时`
-      : `${hours}小时`
+    diff.hour = hours
   }
   if (cmd.indexOf('m') != -1) {
     let minutes = Math.floor(ms / (60 * 1000))
     ms = ms % (60 * 1000) // 除去分钟后剩余的毫秒数
-    res += cpx
-      ? `<span class="salt-time-diff-res salt-time-diff-res-minute">${minutes}</span>分钟`
-      : `${minutes}分钟`
+    diff.minute = minutes
   }
   if (cmd.indexOf('s') != -1) {
     let seconds = Math.floor(ms / 1000)
     ms = ms % 1000 // 除去秒数后剩余的毫秒数
-    res += cpx
-      ? `<span class="salt-time-diff-res salt-time-diff-res-second">${seconds}</span>秒`
-      : `${seconds}秒`
+    diff.second = seconds
   }
-  if (cmd.indexOf('M') != -1 || res.length < 1) {
+  if (cmd.indexOf('M') != -1 || ms === Math.abs(_ms)) {
     // 保险：若指令输入为空，则输出毫秒数
-    res += cpx
-      ? `<span class="salt-time-diff-res salt-time-diff-res-ms">${ms}</span>毫秒`
-      : `${ms}毫秒`
-    // 剩余的毫秒数
+    diff.ms = ms
+  }
+  return readableTimeTxt(diff, cpx)
+}
+function readableTimeTxt(timeObj: ReadableTimeObj, cpx = false): string {
+  let res = cpx
+    ? timeObj.isBefore
+      ? '<span class="salt-time-diff-txt salt-time-diff-before">还有</span>'
+      : '<span class="salt-time-diff-txt salt-time-diff-after">已过去</span>'
+    : ''
+  const loop = [
+    'year',
+    'month',
+    'day',
+    'hour',
+    'minute',
+    'second',
+    'ms',
+  ] as const
+  for (let i = 0; i < loop.length; i++) {
+    const t = loop[i]
+    if (t in timeObj) {
+      res += cpx
+        ? `<span class="salt-time-diff-res salt-time-diff-res-${t}">${timeObj[t]}</span><span class="salt-time-diff-txt salt-time-diff-txt-${t}">${i18n[t]}</span>`
+        : `${timeObj[t]}${i18n[t]}`
+    }
   }
   return res
 }
@@ -82,33 +158,40 @@ function timeDiffHandler() {
   for (let i = 0; i < elems.length; i++) {
     let el = elems[i]
     // txt: [0]起始时间 [1]结束时间，默认为当前时间 [2]
-    let txt = (el.textContent || '')
-      .replace(/[年月日]/g, '/')
-      .replace(/[；：]/g, ':')
-      .split('SPLIT')
-    let time1 = getDate(txt[0])
-    let time2 = getDate(txt[1])
+    const utc = el.classList.contains('utc')
+    let txt = (el.textContent || '').split('SPLIT')
+
+    const t1 = getDate(
+      txt[0].replace(/[年月日]/g, '/').replace(/[；：]/g, ':'),
+      utc
+    )
+    const t2 = getDate(
+      txt[1].replace(/[年月日]/g, '/').replace(/[；：]/g, ':'),
+      utc
+    )
     // 处理指令
     let cmd = 'd'
     if (txt.length > 2) {
       cmd = txt[2]
+        .replace(/年份?/, 'y')
+        .replace(/月份?/, 'o')
         .replace(/[天日]/, 'd')
         .replace(/小?时/, 'h')
         .replace(/分钟?/, 'm')
         .replace('毫秒', 'M')
         .replace('秒', 's')
     }
-    const cps = el.classList.contains('complex')
+    const cpx = el.classList.contains('complex')
     // 送入timeDiff函数
-    const t = timeDiff(time1.getTime() - time2.getTime(), cmd, cps)
-    if (!cps) el.textContent = t
+    const t = timeDiff({ t1, t2, cmd, cpx })
+    if (!cpx) el.textContent = t
     else el.innerHTML = t
     // 后续处理
     el.classList.remove('salt-time-diff')
     el.classList.add('salt-time-diff-done')
     el.setAttribute(cmdAttr, cmd)
-    el.setAttribute(startAttr, time1.toString())
-    el.setAttribute(endAttr, time2.toString())
+    el.setAttribute(startAttr, t1.toString())
+    el.setAttribute(endAttr, t2.toString())
   }
 }
 
@@ -127,14 +210,14 @@ docReady(() => {
       setTimeout(() => {
         // 异步, 避免卡顿
         const el = elems[i]
-        const time1 = getDate(el.getAttribute(startAttr))
-        const time2 = getDate()
+        const t1 = getDate(el.getAttribute(startAttr))
+        const t2 = getDate()
         const cmd = el.getAttribute(cmdAttr) || 'd'
-        const cps = el.classList.contains('complex')
+        const cpx = el.classList.contains('complex')
         // 送入timeDiff函数
-        const t = timeDiff(time1.getTime() - time2.getTime(), cmd, cps)
+        const t = timeDiff({ t1, t2, cmd, cpx })
         // 避免闪烁
-        if (!cps) {
+        if (!cpx) {
           if (el.textContent != t) el.textContent = t
         } else {
           if (el.innerHTML != t) el.innerHTML = t
